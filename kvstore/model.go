7d4ebb497da31d0d6b20953568dead008ceb1274
ZUberMench <zbrandon95@gmail.com>
2019-10-18 18:27:15 -0700

    Removed errormessages.go, added one error message struct to model.go and corresponding anonymous structs, fixed error handling in kvs.go

--- kvstore/model.go
+++ kvstore/model.go
@@ -5,6 +5,27 @@ type Store struct {
 	dal DataAccessLayer
 }
 
+//Anonymous struct used for the ResponseMessage struct
+//This is neccessary for use with omitempty in json object
+exists := struct {
+	Exists bool
+}
+
+//Anonymous struct used for the ResponseMessage struct
+//This is neccessary for use with omitempty in json object
+replaced := struct {
+	Replaced bool
+}
+
+//Error and Success message
+type ResponseMessage struct {
+	Exists  *exists  `json:"doesExist,omitempty"`
+	Error   string  `json:"error,omitempty"`
+	Message string  `json:"message,omitempty"`
+	Replaced *replaced `json:"replaced,omitempty"`
+	Value string    `json:"value,omitempty"`
+}
+
 //DataAccessLayer interface
 type DataAccessLayer interface {
 	Delete(key string) error

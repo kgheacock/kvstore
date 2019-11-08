4579e1377ba5db6b9eda263a785685293b3d9ae0
ZUberMench <zbrandon95@gmail.com>
2019-11-07 17:59:13 -0800

    Correctly creates a ring, distributes virtual nodes across ring, and adds keys

--- hasher/dal.go
+++ hasher/dal.go
@@ -1,77 +1,98 @@
 package hasher
+
 import (
-	"sync"
-	"hash/crc32"
-	"sort"
-	"strings"
+  "sync"
+  "hash/crc32"
+  "sort"
+  "strings"
+  "fmt"
+  "strconv"
 )
 
-type Hasher struct {
-
-}
-
-const NumVirtualNodes = 10
+const NumVirtualNodes = 20
 
 //Ring datastructure
 type Ring struct {
-	sync.Mutex
-	Nodes Nodes
+  sync.Mutex
+  Nodes Nodes
 }
 
 func InitRing() *Ring {
-	return &Ring{Nodes: Nodes{}}
+  return &Ring{Nodes: Nodes{}}
 }
 
 //List of nodes
 type Nodes []*Node
 
+func (n Nodes) Len() int           { return len(n) }
+func (n Nodes) Swap(i, j int)      { n[i], n[j] = n[j], n[i] }
+func (n Nodes) Less(i, j int) bool { return n[i].IdHash < n[j].IdHash }
+
 //Server or Key
 type Node struct {
-	Id string
-	IdHash uint32
+  Id string
+  IdHash uint32
 }
 
 func NewNode(id string) *Node {
-	return &Node{
-		Id:	id,
-		IdHash: HashVal(id),
-	}
+  return &Node{
+    Id:  id,
+    IdHash: HashVal(id),
+  }
 }
 
 func (r *Ring) AddKey (id string){
-	r.Lock()
-	defer r.Unlock()
-	node := NewNode(id)
-	r.Nodes = append(r.Nodes, node)
-	sort.Sort(r.Nodes)
+  r.Lock()
+  defer r.Unlock()
+  node := NewNode(id)
+  r.Nodes = append(r.Nodes, node)
+  sort.Sort(r.Nodes)
 }
 
 func (r *Ring) AddServer (id string) {
-	r.Lock()
-	defer r.Unlock()
-	for i := 0; i < NumVirtualNodes; i++ {
-		fullid := id + ":" + i
-		node := NewNode(id)
-		r.Nodes = append(r.Nodes, node)
-	}
-	sort.Sort(r.Nodes)
+  r.Lock()
+  defer r.Unlock()
+  for i := 0; i < NumVirtualNodes; i++ {
+    fullid := id + ":" + strconv.Itoa(i)
+    node := NewNode(fullid)
+    r.Nodes = append(r.Nodes, node)
+  }
+  sort.Sort(r.Nodes)
 }
 
-func (r *Ring) FindKey (id string) {
-	location := r.search(id)
-	if location >= r.Nodes.Len() {
-		location = 0
-	}
-	serverip := strings.Split(r.Nodes[location].id, ":")
-	return serverip
+func (r *Ring) FindKey (id string) string {
+  //location := sort.Search(r.Nodes.Len(), id)
+  boolfn := func(i int) bool {
+    return r.Nodes[i].IdHash >= HashVal(id)
+  }
+  //if location >= r.Nodes.Len() {
+  //  location = 0
+  //}
+  location := sort.Search(r.Nodes.Len(), boolfn)
+  serverip := strings.Split(r.Nodes[location].Id, ":")
+  return serverip[0]
 }
+
+func (r *Ring) printRing() {
+  for i := 0; i < r.Nodes.Len(); i++ {
+    fmt.Println(r.Nodes[i].Id)
+  }
+}
+
+//func (r *Ring) search(id string) int {
+//  searchfn := func(i int) bool {
+//    return r.Nodes[i].HashId >= hashId(id)
+//  }
+//
+//  return sort.Search(r.Nodes.Len(), searchfn)
+//}
 
 //Takes in string, returns 32bit hash
-func (key string) HashVal (uint32) {
-	return crc32.ChecksumIEEE([]byte(key))
+func HashVal(key string) uint32 {
+  return crc32.ChecksumIEEE([]byte(key))
 }
 
 //Return the ip of a server by passing in the key
-func (h *Hasher) GetServerByKey (key string) (string, error) {
-	return "localhost" , nil //temp
-}
+//func (h *Hasher) GetServerByKey (key string) (string, error) {
+//  return "localhost" , nil //temp
+//}

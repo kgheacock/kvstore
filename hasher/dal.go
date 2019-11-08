f9a4f1992d1b417123f677eac8ee6d319d200ea7
ZUberMench <zbrandon95@gmail.com>
2019-11-07 16:35:23 -0800

    More progress done on consistent hashing implementation

--- hasher/dal.go
+++ hasher/dal.go
@@ -2,15 +2,24 @@ package hasher
 import (
 	"sync"
 	"hash/crc32"
+	"sort"
+	"strings"
 )
+
 type Hasher struct {
 
 }
+
+const NumVirtualNodes = 10
 
 //Ring datastructure
 type Ring struct {
 	sync.Mutex
 	Nodes Nodes
+}
+
+func InitRing() *Ring {
+	return &Ring{Nodes: Nodes{}}
 }
 
 //List of nodes
@@ -22,21 +31,43 @@ type Node struct {
 	IdHash uint32
 }
 
-func (r *Ring) AddNode (id string) {
-	r.Lock()
-	defer r.Unlock()
+func NewNode(id string) *Node {
+	return &Node{
+		Id:	id,
+		IdHash: HashVal(id),
+	}
 }
 
-func (r *Ring) AddKey (id string) {
+func (r *Ring) AddKey (id string){
 	r.Lock()
 	defer r.Unlock()
-
+	node := NewNode(id)
+	r.Nodes = append(r.Nodes, node)
+	sort.Sort(r.Nodes)
 }
 
+func (r *Ring) AddServer (id string) {
+	r.Lock()
+	defer r.Unlock()
+	for i := 0; i < NumVirtualNodes; i++ {
+		fullid := id + ":" + i
+		node := NewNode(id)
+		r.Nodes = append(r.Nodes, node)
+	}
+	sort.Sort(r.Nodes)
+}
 
+func (r *Ring) FindKey (id string) {
+	location := r.search(id)
+	if location >= r.Nodes.Len() {
+		location = 0
+	}
+	serverip := strings.Split(r.Nodes[location].id, ":")
+	return serverip
+}
 
 //Takes in string, returns 32bit hash
-func (key string) HashId (uint32) {
+func (key string) HashVal (uint32) {
 	return crc32.ChecksumIEEE([]byte(key))
 }
 

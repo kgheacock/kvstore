32e32550e8efd91bc7a18cf614c655360dd961f8
ZUberMench <zbrandon95@gmail.com>
2019-11-07 21:18:15 -0800

    Consistent hashing add and get methods succesfully implemented. Segfaults on looking for key that doesnt exist

--- hasher/dal.go
+++ hasher/dal.go
@@ -11,14 +11,31 @@ import (
 
 const NumVirtualNodes = 20
 
+type HasherError struct {
+  Key string
+  Message string
+}
+
+func (e HasherError) Error() string {
+  return fmt.Sprintf("%v: %v", e.Key, e.Message)
+}
+
+func KeyNotFound(key string) error {
+  return HasherError{
+    key,
+    "could not be located.",
+  }
+}
+
 //Ring datastructure
 type Ring struct {
   sync.Mutex
   Nodes Nodes
+  Numkeys int
 }
 
 func InitRing() *Ring {
-  return &Ring{Nodes: Nodes{}}
+  return &Ring{Nodes: Nodes{}, Numkeys: 0}
 }
 
 //List of nodes
@@ -32,21 +49,26 @@ func (n Nodes) Less(i, j int) bool { return n[i].IdHash < n[j].IdHash }
 type Node struct {
   Id string
   IdHash uint32
+  Server *Node
+  IsServer bool
 }
 
-func NewNode(id string) *Node {
+func NewNode(id string, nodetype bool) *Node {
   return &Node{
     Id:  id,
     IdHash: HashVal(id),
+    Server: nil,
+    IsServer: nodetype,
   }
 }
 
 func (r *Ring) AddKey (id string){
   r.Lock()
   defer r.Unlock()
-  node := NewNode(id)
+  node := NewNode(id, false)
   r.Nodes = append(r.Nodes, node)
   sort.Sort(r.Nodes)
+  r.Numkeys++
 }
 
 func (r *Ring) AddServer (id string) {
@@ -54,45 +76,90 @@ func (r *Ring) AddServer (id string) {
   defer r.Unlock()
   for i := 0; i < NumVirtualNodes; i++ {
     fullid := id + ":" + strconv.Itoa(i)
-    node := NewNode(fullid)
+    node := NewNode(fullid, true)
     r.Nodes = append(r.Nodes, node)
   }
   sort.Sort(r.Nodes)
 }
 
-func (r *Ring) FindKey (id string) string {
-  //location := sort.Search(r.Nodes.Len(), id)
+func (r *Ring) GetKeyNode (id string) (*Node, error) {
   boolfn := func(i int) bool {
     return r.Nodes[i].IdHash >= HashVal(id)
   }
-  //if location >= r.Nodes.Len() {
-  //  location = 0
-  //}
   location := sort.Search(r.Nodes.Len(), boolfn)
-  serverip := strings.Split(r.Nodes[location].Id, ":")
-  return serverip[0]
+  if location >= r.Nodes.Len() {
+    location = 0
+  }
+  if location == -1 { //***THIS DOES NOT CURRENTLY WORK***
+    return &Node{}, KeyNotFound("")
+  }
+  return r.Nodes[location], nil
 }
 
-func (r *Ring) printRing() {
+func (r *Ring) ReShard () {
+  for i := 0; i < r.Nodes.Len(); i++ {
+    if r.Nodes[i].IsServer == false {
+      for j := i; j < r.Nodes.Len(); j++ {
+        //If we reach end and didnt find server
+        if j == r.Nodes.Len() - 1 {
+          j = 0
+        }
+        if r.Nodes[j].IsServer == true {
+          r.Nodes[i].Server = r.Nodes[j]
+          break
+        }
+      }
+    }
+  }
+}
+
+func (r *Ring) GetNumOfKeys () int {return r.Numkeys}
+
+func (r *Ring) printRing () {
   for i := 0; i < r.Nodes.Len(); i++ {
     fmt.Println(r.Nodes[i].Id)
   }
 }
 
-//func (r *Ring) search(id string) int {
-//  searchfn := func(i int) bool {
-//    return r.Nodes[i].HashId >= hashId(id)
-//  }
-//
-//  return sort.Search(r.Nodes.Len(), searchfn)
-//}
-
 //Takes in string, returns 32bit hash
-func HashVal(key string) uint32 {
+func HashVal (key string) uint32 {
   return crc32.ChecksumIEEE([]byte(key))
 }
 
 //Return the ip of a server by passing in the key
-//func (h *Hasher) GetServerByKey (key string) (string, error) {
-//  return "localhost" , nil //temp
-//}
+func (r *Ring) GetServerByKey (key string) (string, error) {
+  node, err := r.GetKeyNode(key)
+  if err != nil {
+    return "could not locate key.", KeyNotFound(key)
+  }
+  serverip := strings.Split(node.Server.Id, ":")
+  return serverip[0], nil
+}
+/*
+//TESTING CODE
+func main(){
+  keys := []string{"Chris", "Brandon", "Colby", "Keith", "Alvaro", "Mackey"}
+  
+  myRing := InitRing()
+  myRing.AddServer("A")
+  myRing.AddServer("B")
+  myRing.AddServer("C")
+
+  for i := 0; i < len(keys); i++ {
+    myRing.AddKey(keys[i]) 
+  }
+  
+  myRing.printRing()
+  myRing.ReShard()
+
+  for i := 0; i < len(keys); i++ {
+    theNodeWanted, _ := myRing.GetServerByKey(keys[i])
+    fmt.Println("Key:", keys[i]+ ",","is on Server: ",theNodeWanted)
+  }
+  
+  //This breaks everything
+  theNodeWanted, _ := myRing.GetServerByKey("DoesntExist")
+  fmt.Println("Key:", "DoesntExist" + ",","is on Server: ",theNodeWanted)
+  
+}
+*/

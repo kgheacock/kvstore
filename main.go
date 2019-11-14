349e157663d7e597d3da39120723b2eeb5fc6712
Colby Leiske <colby.leiske@gmail.com>
2019-11-14 12:18:31 -0800

    cleaned up copy pasted code comments

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ func main() {
 	}
 
 	log.Printf("Starting on %s\n", addr)
-	// Run our server in a goroutine so that it doesn't block.
+
 	go func() {
 		if err := srv.ListenAndServe(); err != nil {
 			log.Println(err)
@@ -47,22 +47,13 @@ func main() {
 	}()
 
 	c := make(chan os.Signal, 1)
-	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
-	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
 	signal.Notify(c, os.Interrupt)
 
-	// Block until we receive our signal.
 	<-c
 
-	// Create a deadline to wait for.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
 	defer cancel()
-	// Doesn't block if no connections, but will otherwise wait
-	// until the timeout deadline.
 	srv.Shutdown(ctx)
-	// Optionally, you could run srv.Shutdown in a goroutine and block on
-	// <-ctx.Done() if your application should wait for other services
-	// to finalize based on context cancellation.
 	log.Println("shutting down")
 	os.Exit(0)
 

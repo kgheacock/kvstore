0782a1d8cba700668f5b5fd1b8c1c6374de20cd2
cortiz319 <chanorti@ucsc.edu>
2019-11-15 17:26:44 -0800

    working on python test file

--- temp.sh
+++ temp.sh
@@ -1,15 +1,9 @@
 export ADDRESS="localhost:8080"
-export VIEW="localhost:8080,localhost:8081,localhost:8082, localhost:8083, localhost:8084"
+export VIEW="localhost:8080,localhost:8081,localhost:8082"
 go run main.go &
 export ADDRESS="localhost:8081"
-export VIEW="localhost:8080,localhost:8081,localhost:8082, localhost:8083, localhost:8084"
+export VIEW="localhost:8080,localhost:8081,localhost:8082"
 go run main.go &
 export ADDRESS="localhost:8082"
-export VIEW="localhost:8080,localhost:8081,localhost:8082, localhost:8083, localhost:8084"
-go run main.go &
-export ADDRESS="localhost:8083"
-export VIEW="localhost:8080,localhost:8081,localhost:8082, localhost:8083, localhost:8084"
-go run main.go &
-export ADDRESS="localhost:8084"
-export VIEW="localhost:8080,localhost:8081,localhost:8082, localhost:8083, localhost:8084"
-go run main.go
+export VIEW="localhost:8080,localhost:8081,localhost:8082"
+go run main.go 

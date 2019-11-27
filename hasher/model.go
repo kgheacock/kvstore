91071d6b7513be9fd98d5d5481ccf08020f3a657
Keith Heacock <kheacock2@gmail.com>
2019-11-27 13:15:04 -0800

    added Quorom struct to hasher model

--- hasher/model.go
+++ hasher/model.go
@@ -8,6 +8,34 @@ type DataAccessLayer interface {
 	GetServerByKey(key string) (string, error)
 	Servers() []string
 	AddServer(ip string)
+	GetQuorum(name string) (Quorum, error)
+}
+type Quorum struct {
+	Name    string
+	members []string
+}
+
+func (q *Quorum) AddMember(ip string) {
+	q.members = append(q.members, ip)
+}
+func (q *Quorum) RemoveMember(ip string) error{
+	if len(q.members) == 0{
+		return Errors.new("Element not found") //Is this correct?
+	}
+	if elem == q.members[0]{
+		return nil
+	}
+	for i,elem := q.members{
+		if(elem == ip){
+			q.members = append(q.members[:i-1],q.members[i+1:])
+			return nil
+		}
+	}
+	return Errors.new("Element not found")
+}
+
+func (q *Quorum) SetMembers(ips [] string){
+	q.members = ips
 }
 
 func NewRingStore(dal DataAccessLayer) *Store {
